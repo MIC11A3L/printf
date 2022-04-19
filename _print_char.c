6ae471806e82865f2efaecd08bcf84c8421702d3
MIC11A3L <99387901+MIC11A3L@users.noreply.github.com>
2022-04-19 12:50:39 +0100

    char function fix

--- _print_char.c
+++ _print_char.c
@@ -1,1 +1,14 @@
-lk
+#include "main.h"
+
+/**
+ * _print_char - writes the character c to stdout
+ * @ap: The character to print
+ *
+ * Return: 1
+ */
+
+int _print_char(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+	return (1);
+}
